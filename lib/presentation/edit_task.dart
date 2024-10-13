ace86f8810e20798780332366daedf56374dff39
Mersad <nickmersad81@gmail.com>
2024-10-13 18:55:12 +0330

    add task screen and share botton

--- lib/presentation/edit_task.dart
+++ lib/presentation/edit_task.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/data/data.dart';
 import 'package:flutter_todo/main.dart';
@@ -22,35 +23,125 @@ class EditTasksScreen extends StatelessWidget {
   /// If the [Task] is already in the box, [Task.save] is called to update the
   /// task in the box.
   Widget build(BuildContext context) {
-    TextEditingController _controller = TextEditingController();
-    return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          final task = Task();
-          task.name = _controller.text;
-          task.priority = Priority.low;
-          if (task.isInBox) {
-            task.save();
-          } else {
-            final Box<Task> box = Hive.box(taskBoxName);
-            box.add(task);
-          }
+    TextEditingController controller = TextEditingController();
+    return SafeArea(
+      bottom: false,
+      left: false,
+      right: false,
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            final task = Task();
+            task.name = controller.text;
+            task.priority = Priority.low;
+            if (task.isInBox) {
+              task.save();
+            } else {
+              final Box<Task> box = Hive.box(taskBoxName);
+              box.add(task);
+            }
 
-          Navigator.pop(context);
-        },
-        label: const Text("Save Changes"),
-      ),
-      appBar: AppBar(
-        title: const Text('Edit Task'),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _controller,
-            decoration:
-                const InputDecoration(label: Text("Add Task for Today")),
-          )
-        ],
+            Navigator.pop(context);
+          },
+          label: const Row(
+            children: [
+              Text("Save Changes"),
+              SizedBox(
+                width: 4,
+              ),
+              Icon(
+                CupertinoIcons.checkmark,
+                size: 18,
+              )
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                height: 112,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      primaryColor,
+                      primaryColor.withOpacity(0.5),
+                    ],
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Icon(
+                              CupertinoIcons.back,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 94,
+                          ),
+                          const Text("Add Task",
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 10, 16, 100),
+                child: Container(
+                  margin: const EdgeInsets.only(top: 8),
+                  height: 84,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 4,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: TextField(
+                      controller: controller,
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            CupertinoIcons.add_circled,
+                            color: secondryTextColor,
+                          ),
+                          label: Text(
+                            "Add Task for Today",
+                          ),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -26,156 +26,181 @@ class HommeScreen extends StatelessWidget {
   /// [Padding] with a [Task] widget as its child.
   Widget build(BuildContext context) {
     Box<Task> box = Hive.box(taskBoxName);
-    return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const EditTasksScreen()));
-          },
-          label: const Row(
-            children: [
-              Text("Add Task"),
-              SizedBox(
-                width: 4,
-              ),
-              Icon(CupertinoIcons.add_circled)
-            ],
-          )),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 102,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    primaryColor,
-                    primaryColor.withOpacity(0.5),
-                  ],
+    return SafeArea(
+      bottom: false,
+      left: false,
+      right: false,
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const EditTasksScreen()));
+            },
+            label: const Row(
+              children: [
+                Text("Add Task"),
+                SizedBox(
+                  width: 4,
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("To Do List",
-                            style: TextStyle(
-                                fontSize: 25,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold)),
-                        Icon(CupertinoIcons.share_up, color: Colors.white),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 38,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(19),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: primaryColor.withOpacity(0.1),
-                              blurRadius: 0,
-                            )
-                          ]),
-                      child: const TextField(
-                        textAlign: TextAlign.start,
-                        decoration: InputDecoration(
-                          iconColor: secondryTextColor,
-                          labelStyle: TextStyle(color: secondryTextColor),
-                          hintText: "Search Tasks",
-                          prefixIcon: Icon(CupertinoIcons.search),
-                          border: InputBorder.none,
+                Icon(CupertinoIcons.add_circled)
+              ],
+            )),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            children: [
+              Container(
+                height: 112,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      primaryColor,
+                      primaryColor.withOpacity(0.5),
+                    ],
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("To Do List",
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          InkWell(
+                              onTap: () {
+                                final snackBar = SnackBar(
+                                  content: const Text('Share Succesfully!'),
+                                  action: SnackBarAction(
+                                    backgroundColor: primaryColor,
+                                    label: 'Undo',
+                                    textColor: Colors.white,
+                                    onPressed: () {
+                                      // Some code to undo the change.
+                                    },
+                                  ),
+                                );
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              },
+                              child: const Icon(CupertinoIcons.share_up,
+                                  color: Colors.white)),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        height: 38,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(19),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: primaryColor.withOpacity(0.1),
+                                blurRadius: 0,
+                              )
+                            ]),
+                        child: const TextField(
+                          textAlign: TextAlign.start,
+                          decoration: InputDecoration(
+                            iconColor: secondryTextColor,
+                            labelStyle: TextStyle(color: secondryTextColor),
+                            hintText: "Search Tasks",
+                            prefixIcon: Icon(CupertinoIcons.search),
+                            border: InputBorder.none,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: ValueListenableBuilder<Box<Task>>(
-                builder: (context, box, child) {
-                  return ListView.builder(
-                      padding: const EdgeInsets.fromLTRB(16, 10, 16, 100),
-                      itemCount: box.values.length + 1,
-                      itemBuilder: (context, index) {
-                        if (index == 0) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const Text(
-                                      'Today',
-                                      style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Container(
-                                      height: 3,
-                                      width: 50,
-                                      decoration: BoxDecoration(
-                                          color: primaryColor,
-                                          borderRadius:
-                                              BorderRadius.circular(10)),
-                                    )
-                                  ],
-                                ),
-                                MaterialButton(
-                                  elevation: 0,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  textColor: secondryTextColor,
-                                  color: const Color(0xffEAEFF5),
-                                  onPressed: () {
-                                    box.clear();
-                                  },
-                                  child: const Row(
+              Expanded(
+                child: ValueListenableBuilder<Box<Task>>(
+                  builder: (context, box, child) {
+                    return ListView.builder(
+                        padding: const EdgeInsets.fromLTRB(16, 10, 16, 100),
+                        itemCount: box.values.length + 1,
+                        itemBuilder: (context, index) {
+                          if (index == 0) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
                                     children: [
-                                      Text("Delete All"),
-                                      SizedBox(
-                                        width: 4,
+                                      const Text(
+                                        'Today',
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold),
                                       ),
-                                      Icon(
-                                        CupertinoIcons.delete,
-                                        color: secondryTextColor,
-                                        size: 15,
-                                      ),
+                                      Container(
+                                        height: 3,
+                                        width: 50,
+                                        decoration: BoxDecoration(
+                                            color: primaryColor,
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                      )
                                     ],
                                   ),
-                                )
-                              ],
-                            ),
-                          );
-                        } else {
-                          final Task task = box.values.toList()[index - 1];
-                          return Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: TaskItem(task: task),
-                          );
-                        }
-                      });
-                },
-                valueListenable: box.listenable(),
+                                  MaterialButton(
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    textColor: secondryTextColor,
+                                    color: const Color(0xffEAEFF5),
+                                    onPressed: () {
+                                      box.clear();
+                                    },
+                                    child: const Row(
+                                      children: [
+                                        Text("Delete All"),
+                                        SizedBox(
+                                          width: 4,
+                                        ),
+                                        Icon(
+                                          CupertinoIcons.delete,
+                                          color: secondryTextColor,
+                                          size: 15,
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          } else {
+                            final Task task = box.values.toList()[index - 1];
+                            return Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: TaskItem(task: task),
+                            );
+                          }
+                        });
+                  },
+                  valueListenable: box.listenable(),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
